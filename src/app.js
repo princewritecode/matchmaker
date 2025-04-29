21cf5a12eaf9506541b9debaa0518c72949ae045
princewritecode <princewritecode@gmail.com>
2025-04-29 16:19:55 +0530

    exploring different route get post update patch

--- src/app.js
+++ src/app.js
@@ -1,20 +1,40 @@
 // core js file whole node code
 //express js to create server
-
 const express = require('express');
 // creating new application of express
 const app = new express();
 // now i have to listen on the port
 // handle a request
 
-app.use("/test", (req, res) =>
+//remember always the order of the route matters
+// app.use("/user", (req, res) =>
+// {
+//     res.send('ha ha ha ha ...');
+// });
+
+// This will only handle the request get
+app.get("/user", (req, res) =>
 {
-    res.send('hello from the server...');
+    res.send({ firstName: 'prince', lastName: 'patel' });
 });
-app.use("/hello", (req, res) =>
+// this will handle the post request
+
+app.post("/user", (req, res) =>
 {
-    res.send('welcome to hello.....');
+    // saving data to the database
+    // console.log("save data to the database...");
+    res.send("data successfully saved to database...");
 });
+
+// app.use("/hello", (req, res) =>
+// {
+//     res.send('welcome to hello.....');
+// });
+
+// app.use("/", (req, res) =>
+// {
+//     res.send('welcome to home page..');
+// });
 app.listen(3000, () =>
 {
     console.log('server is successfully running on port 3000...');
