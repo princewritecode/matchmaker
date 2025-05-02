6485765602ff3a9545042f8b62ff32defef2c35c
princewritecode <princewritecode@gmail.com>
2025-05-02 14:06:41 +0530

    exploring how to connect with database

--- src/app.js
+++ src/app.js
@@ -1,41 +1,36 @@
-// core js file whole node code
-//express js to create server
+// core js file whole node code //express js to create server
 const express = require('express');
+const connectDb = require('./config/database');
 // creating new application of express
-const app = new express();
-// now i have to listen on the port
-// handle a request
-
-//remember always the order of the route matters
-// app.use("/user", (req, res) =>
-// {
-//     res.send('ha ha ha ha ...');
-// });
-
-// This will only handle the request get
-app.get("/user", (req, res) =>
+const app = express(); // Fix: don't use 'new' with express()
+const User = require('./model/user');
+app.post('/signup', async (req, res) =>
 {
-    res.send({ firstName: 'prince', lastName: 'patel' });
-});
-// this will handle the post request
+    const userObj = {
+        firstName: 'prince',
+        lastName: 'patel',
+        emailId: 'princepatel.dev@gmail.com',
+        password: 'prince@123'
+    };
+    // creating new instance of the user model
+    const user = new User(userObj);
 
-app.post("/user", (req, res) =>
-{
-    // saving data to the database
-    // console.log("save data to the database...");
-    res.send("data successfully saved to database...");
+    // below function will return promise most of the api will return a promise
+    await user.save();
+    res.send('user added successfully');
+
 });
 
-// app.use("/hello", (req, res) =>
-// {
-//     res.send('welcome to hello.....');
-// });
 
-// app.use("/", (req, res) =>
-// {
-//     res.send('welcome to home page..');
-// });
-app.listen(3000, () =>
+connectDb().then(() =>
 {
-    console.log('server is successfully running on port 3000...');
+    console.log('connected successfully');
+    app.listen(3000, () =>
+    {
+        console.log('server is successfully running on port 3000...');
+    });
+}).catch((err) =>
+{
+    console.log(err);
 });
+
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+// create schema and talking to mongodb we need mongoose
+const mongoose = require('mongoose');
+const connectDb = async () =>
+{
+    await mongoose.connect('mongodb+srv://princepateldev:7747871218@hellonode.oifbthx.mongodb.net/?retryWrites=true&w=majority&appName=hellonode');
+};
+module.exports = connectDb;
