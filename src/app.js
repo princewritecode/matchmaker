eace6c0bb4b71c69cb956194d267166083dd0a80
princewritecode <princewritecode@gmail.com>
2025-05-07 12:16:15 +0530

    update delete on routes

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ app.post('/signup', async (req, res) =>
     await user.save();
     res.send('user added successfully');
 });
-
 app.get('/user', async (req, res) =>
 {
     const userEmail = req.body.emailId;
@@ -49,6 +48,35 @@ app.get('/feed', async (req, res) =>
     catch (err)
     {
         console.log(err);
+    }
+});
+// delete user by id
+app.delete('/user', async (req, res) =>
+{
+    const userId = req.body.userId;
+    try
+    {
+        const user = await (User.findOneAndDelete(userId));
+        res.send('deleted user successfully...');
+    }
+    catch (err)
+    {
+        res.status(400).send('something went wrong...');
+    }
+});
+// update user
+app.patch('/user', async (req, res) =>
+{
+    const userId = req.body.userId;
+    const data = req.body;
+    try
+    {
+        await User.findByIdAndUpdate({ _id: userId }, data);
+        res.send('user updated successfully...');
+    }
+    catch (err)
+    {
+        res.status(400).send('something went wrong...');
     }
 });
 connectDb().then(() =>
